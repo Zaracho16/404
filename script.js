3da5c0a3ba33fabb43a124df50548ca8624754d4
Adrian Zaracho <adrianzaracho16@gmail.com>
2025-07-08 03:22:11 -0300

    Añadi la funcionalidad de agregar los productos al carrito

--- script.js
+++ script.js
@@ -41,3 +41,86 @@ function disminuir() {
     cantidadSpan.textContent = cantidad;
   }
 }
+
+//-----Script para abrir y cerrar el carrito de compras-----
+const iconoCarrito = document.getElementById('icono-carrito');
+const carritoLateral = document.querySelector('.carrito-lateral');
+const botonCerrar = document.getElementById('cerrar-carrito');
+const fondoOscuro = document.getElementById('fondo-oscuro');
+
+iconoCarrito.addEventListener('click', () => {
+  carritoLateral.classList.toggle('active');
+  fondoOscuro.classList.add('active');
+});
+
+botonCerrar.addEventListener('click', () => {
+  carritoLateral.classList.remove('active');
+  fondoOscuro.classList.remove('active');
+});
+
+fondoOscuro.addEventListener('click', () => {
+  carritoLateral.classList.remove('active');
+  fondoOscuro.classList.remove('active');
+});
+
+
+
+// -----Script para añadir productos al carrito de compras-----
+
+let carrito = [];
+
+document.querySelectorAll('.boton-carrito').forEach(btn => {
+  btn.addEventListener('click', () => {
+    const cuadro = btn.closest('.cuadro-perfumes-carolinaHerrera');
+    const nombre = cuadro.querySelector('h3').textContent.trim();
+    const imagenSrc = cuadro.querySelector("img").src;
+    const precioTexto = cuadro.querySelector('span').textContent.trim();
+    const precio = parseInt(precioTexto.replace(/\D/g, '')); // Solo números
+
+    carrito.push({ nombre, precio, imagenSrc });
+    actualizarCarrito();
+  });
+});
+
+function actualizarCarrito() {
+  const lista = document.getElementById('carrito-lista');
+  const totalSpan = document.getElementById('total-carrito');
+  lista.innerHTML = '';
+  let total = 0;
+
+  carrito.forEach((producto, index) => {
+    total += producto.precio;
+    const li = document.createElement('li');
+    li.classList.add('carrito-item'); // clase para estilo
+
+    li.innerHTML = `
+    <div class="producto-contenedor-carritoCompra">
+
+      <div class="caja-img-carrito">
+        <img src="${producto.imagenSrc}" alt="${producto.nombre}" class="estilo-img-carrito">
+      </div>
+
+      <div class="caja-nombre-carrito">
+        <span class="nombreProducto-carrito">${producto.nombre} - ${producto.precio.toLocaleString()} Gs </span>
+      </div>
+
+    </div>
+      
+      <button onclick="eliminarDelCarrito(${index})" class="icono-eliminarProductoCarrito">❌</button>
+    `;
+    lista.appendChild(li);
+  });
+
+  totalSpan.textContent = total.toLocaleString();
+}
+
+function eliminarDelCarrito(index) {
+  carrito.splice(index, 1);
+  actualizarCarrito();
+}
+
+document.getElementById('vaciar-carrito').addEventListener('click', () => {
+  carrito = [];
+  actualizarCarrito();
+});
+
