299eb9e4640374a6d456bfffdec224dcf4ac513b
Adrian Zaracho <adrianzaracho16@gmail.com>
2025-07-27 02:42:59 -0300

    Cambios minimos

--- js/cargarFooter.js
+++
@@ -1,9 +0,0 @@
-
-document.addEventListener("DOMContentLoaded", () => {
-  fetch("../footer.html")
-    .then(res => res.text())
-    .then(data => {
-      document.getElementById("footer").innerHTML = data;
-    })
-    .catch(err => console.error("Error al cargar el footer:", err));
-});
