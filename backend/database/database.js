ac0822408ea39a938b830808ffc2b405e5511244
Adrian Zaracho <adrianzaracho16@gmail.com>
2026-07-29 18:05:52 -0300

    Cambio de conexion mysql a pool

--- backend/database/database.js
+++ backend/database/database.js
@@ -1,18 +1,22 @@
 
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
 
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT,
+
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 
 });
 
 
-db.connect((error) => {
+db.getConnection((error, connection) => {
 
     if(error){
         console.log("Error conectando a la BD:", error);
@@ -20,6 +24,8 @@ db.connect((error) => {
     }
 
     console.log("Base de datos conectada");
+
+    connection.release();
 
 });
 
