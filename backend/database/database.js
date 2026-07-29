8bf914a83f4b1a23fdd0a970fed359eb85cfd580
Adrian Zaracho <adrianzaracho16@gmail.com>
2026-07-29 17:26:02 -0300

    configuracion de conexion a db mediante variables de entorno

--- backend/database/database.js
+++ backend/database/database.js
@@ -1,11 +1,27 @@
 
 const mysql = require("mysql2");
 
-const db = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "2025",
-    database: "404"
+const db = mysql.createConnection({
+
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
+
 });
+
+
+db.connect((error) => {
+
+    if(error){
+        console.log("Error conectando a la BD:", error);
+        return;
+    }
+
+    console.log("Base de datos conectada");
+
+});
+
 
 module.exports = db;
