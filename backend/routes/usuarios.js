a88ff2c4e672f33e2b207ce6e687c2305231caf8
Adrian Zaracho <adrianzaracho16@gmail.com>
2026-08-04 00:21:26 -0300

    feat: se agrego para que las claves esten encriptadas

--- backend/routes/usuarios.js
+++ backend/routes/usuarios.js
@@ -1,4 +1,6 @@
 
+const bcrypt = require("bcrypt");
+
 const express = require("express");
 
 const router = express.Router();
@@ -6,17 +8,18 @@ const router = express.Router();
 const db = require ("../database/database");
 const { route } = require("./products");
 
-router.post("/registro", (req, res) => {
+router.post("/registro", async (req, res) => {
 
     const { nombre, email, password } = req.body;
 
+    const passwordEncriptada = await bcrypt.hash(password, 10);
 
     const sql = `
         INSERT INTO usuarios (nombre, email, password)
         VALUES ( ?, ?, ? );
     `
 
-    db.query(sql, [nombre, email, password], (error, resultado) => {
+    db.query(sql, [nombre, email, passwordEncriptada], (error, resultado) => {
         
         if(error) {
             console.log(error);
