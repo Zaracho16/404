21db0e5dabe2ec44b775ad5afe768a45334327b5
Adrian Zaracho <adrianzaracho16@gmail.com>
2026-08-08 22:35:48 -0300

    feat: se añadio la funcionalidad para el login

---
+++ js/inicioSesion.js
@@ -0,0 +1,48 @@
+
+const API_URL = "https://four04-8o6t.onrender.com";
+
+const formulario = document.getElementById("login-form");
+
+formulario.addEventListener("submit", async(e) => {
+
+    e.preventDefault();
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    console.log("email: " + email);
+    console.log("clave: " + password);
+
+    try {
+
+        const respuesta = await fetch(`${API_URL}/usuarios/login`, {
+
+            method = "POST",
+
+            headers: {
+                "Content-Type": "application/json"
+            },
+
+            body: JSON.stringify({
+                email,
+                password
+            })
+
+        });
+
+        const datos = await respuesta.json();
+
+        console.log("Respuesta del server: ", datos);
+
+        alert(datos.mensaje);
+
+    } catch(error) {
+
+        console.log("error: ", error);
+
+        alert("no se pudo conectar con el servidor");
+
+    }
+
+
+});
